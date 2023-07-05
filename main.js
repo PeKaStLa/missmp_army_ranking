8a667aee6cffb1867af2ec15c4bd41eea5745a47
Peter Stadler <14peterstadler@gmail.com>
2023-07-06 00:26:26 +0200

    add officer class, constructor and 3 initial test officers

--- main.js
+++ main.js
@@ -5,5 +5,25 @@
     by Peter Stadler
     05.07.2023, 22:45 Uhr
 */
-var test3 = "main.ts works test3";
-console.log(test3);
+var test4 = "main.ts works test3";
+console.log(test4);
+/*
+interface Officer {
+    readonly id: number;
+    readonly name: string;
+    subordinates: Officer[]
+}*/
+var Officer = /** @class */ (function () {
+    function Officer(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return Officer;
+}());
+var officers = [];
+officers.push(new Officer(12, "Peter"));
+officers.push(new Officer(14, "An"));
+officers.push(new Officer(16, "Johannes"));
+officers.forEach(function (element) {
+    console.log(element.name);
+});
--- main.ts
+++ main.ts
@@ -9,16 +9,39 @@
 let test4: string = "main.ts works test3";
 console.log(test4);
 
+/*
 interface Officer {
     readonly id: number;
     readonly name: string;
     subordinates: Officer[]
-    }
-    
+}*/
+
+class Officer {
+    id: number;
+    name: string;
+    subordinates: Officer[];
+
+    constructor(id: number, name: string) {
+        this.id =  id;
+        this.name = name;
+      }
+}
+
 interface ArmyRankingApp {
     general: Officer
     moveOfficer(officerID: number, managerID: number): void
     undo(): void
     redo(): void
-    }
-    
+}
+
+
+let officers: Array<Officer> = [];
+
+//initial test objects
+officers.push(new Officer(12, "Peter"));
+officers.push(new Officer(14, "An"));
+officers.push(new Officer(16, "Johannes"));
+
+officers.forEach(element => {
+    console.log(element.name);
+});
