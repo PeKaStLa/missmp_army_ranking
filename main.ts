389512c7b167cbe6c67da59ed3286547c242381c
Peter Stadler <14peterstadler@gmail.com>
2023-07-05 23:48:42 +0200

    compiling ts into js works with tsc cli command

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@
     05.07.2023, 22:45 Uhr
 */
 
+// let message: string = 'main.ts works';
+console.log('main.ts works');
+
 interface Officer {
     readonly id: number;
     readonly name: string;
