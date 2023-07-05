ab594ee2b692eeb898c07171fbb24e60916b6dee
Peter Stadler <14peterstadler@gmail.com>
2023-07-05 23:37:34 +0200

    add interface officer and ArmyRankingApp

--- main.ts
+++ main.ts
@@ -6,3 +6,16 @@
     05.07.2023, 22:45 Uhr
 */
 
+interface Officer {
+    readonly id: number;
+    readonly name: string;
+    subordinates: Officer[]
+    }
+    
+interface ArmyRankingApp {
+    general: Officer
+    moveOfficer(officerID: number, managerID: number): void
+    undo(): void
+    redo(): void
+    }
+    
